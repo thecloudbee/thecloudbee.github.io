e1b930693fc8d696f1a10a5362a58c2283ca9dea
amrojsandhu <amrojpals@gmail.com>
2020-12-20 14:05:13 +0530

    #thecloudbee: Image resize automation

--- automation/src/utils/image_resize.py
+++ automation/src/utils/image_resize.py
@@ -1,13 +1,10 @@
-import sys
+import os
 
 from PIL import Image
-import PIL
-import os
-import glob
 
 
 # https://sempioneer.com/python-for-seo/how-to-compress-images-in-python/
-def compress_images(directory):
+def resize_images(directory):
     os.chdir(directory)
     files = os.listdir()
     images = [file for file in files if file.endswith(('jpg', 'png'))]
@@ -17,18 +14,28 @@ def compress_images(directory):
         x, y = img.size
         print(f'Initial image: {image} with size: {x}, {y}')
         if x < 200 or y < 200:
-            print(f'Skipping compressing for image: {image}. Reason, too small.')
+            print(f'#thecloudbee Skipping resize for image: {image}. Reason, too small.')
+            continue
+        elif x == 1200:
+            print(f'#thecloudbee Skipping resize for image: {image}. Reason, already in-size.')
             continue
         base_width = 1200
         wpercent = (base_width / float(img.size[0]))
         height = int((float(img.size[1]) * float(wpercent)))
         img = img.resize((base_width, height), Image.ANTIALIAS)
-        print(f'Final image: {image} with size: {base_width}, {height}')
+        print(f'#thecloudbee Final image: {image} with size: {base_width}, {height}')
 
-        # image_size_in_kb = int(sys.getsizeof(img.tobytes())/100)
-        # quality = int(100 * 100 / image_size_in_kb)
-
-        img.save(image, optimize=True, quality=50)
+        img.save(image, optimize=True, quality=80)
 
 
-compress_images('/Users/amroj/IdeaProjects/thecloudbee.github.io/assets/images/2021-01-01')
+def resize_recursive(parent_directory):
+    print(f'Current directory: {os.getcwd()}')
+    print(f'#thecloudbee Starting process of image resize')
+    for i in os.scandir(parent_directory):
+        if i.is_dir():
+            print(f'Processing folder: {i.path}')
+            resize_images(i.path)
+    print(f'#thecloudbee Ending process of image resize')
+
+
+resize_recursive('../../../assets/images/')
